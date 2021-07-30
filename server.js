8b2b33ed25ae17507ca76cc77877357edc40ba4e
msimp <max.simpson@gmail.com>
2021-07-30 18:07:31 -0500

    please

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, './notes.html'));
 });
 
 
