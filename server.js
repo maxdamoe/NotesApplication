76cf8dc00947d9e4c87ff8c2dfa2409135a23d5e
msimp <max.simpson@gmail.com>
2021-07-30 18:02:24 -0500

    jesus christ

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
 
 //==========GET Routes =======
 
