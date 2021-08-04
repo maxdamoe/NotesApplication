00b7021181b5c55d41f9048183b85fe7436ea19f
msimp <max.simpson@gmail.com>
2021-08-04 12:48:34 -0500

    Progress on Server.js

--- server.js
+++ server.js
@@ -1,71 +1,118 @@
-const fs = require("fs");
-const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
+const fs = require("fs")
+const path = require("path")
+
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
 app.use(express.json());
-const { notes } = require("./data/db");
+app.use(express.static("public"));
 
-function findById(id, notesArray) {
-  const result = notesArray.filter((note) => note.id === id)[0];
-  return result;
-}
+const notesArray = [];
 
-function createNewNote(body, notesArray) {
-  console.log(body);
-  // our function's main code will go here!
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "./data/db.json"),
-    JSON.stringify({ notes: notesArray }, null, 2)
-  );
 
-  // return finished code to post route for response
-  return note;
-}
 
-//------ GET ROUTES------
 
-app.get("/api/db/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  res.json(result);
-});
 
-app.get("/api/db", (req, res) => {
-  res.json(notes);
-});
+//HTML Routes
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
+
+// API Routes
+ app.get('/api/notes', (req, res) => {
+   return res.json(notesArray)
+ });
+
+ app.post('/api/notes', (req, res) => {
+ const note = req.body;
+ note.id = notesArray.length.toString();
+ notesArray.push(note);
+
+ fs.writeFile('data/db.json', JSON.stringify(notesArray), (err) => {
+   if (err) console.log(err);
+ })
+  res.json(notesArray);
+})
+
+
+
+// const fs = require("fs");
+// const path = require("path");
+// const express = require("express");
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
+// const { notes } = require("./data/db");
+
+// function findById(id, notesArray) {
+//   const result = notesArray.filter((note) => note.id === id)[0];
+//   return result;
+// }
+
+// function createNewNote(body, notesArray) {
+//   console.log(body);
+//   // our function's main code will go here!
+//   const note = body;
+//   notesArray.push(note);
+//   fs.writeFileSync(
+//     path.join(__dirname, "./data/db.json"),
+//     JSON.stringify({ notes: notesArray }, null, 2)
+//   );
+
+//   // return finished code to post route for response
+//   return note;
+// }
+
+// //------ GET ROUTES------
+
+// app.get("/api/db/:id", (req, res) => {
+//   const result = findById(req.params.id, notes);
+//   res.json(result);
+// });
+
+// app.get("/api/db", (req, res) => {
+//   res.json(notes);
+// });
+
 // app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, './notes.html'));
+//   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+// });
+
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+// });
+
+// // app.get('/', (req, res) => {
+// //   res.sendFile(path.join(__dirname, './notes.html'));
+// // });
+
+
+// //==========GET Routes =======
+
+// app.listen(PORT, () => {
+//   console.log(`API server now on port ${PORT}!`);
 // });
 
 
-//==========GET Routes =======
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
-
-
-
-app.post("/api/db", (req, res) => {
-  // req.body is where our incoming content will be
-  console.log(req.body);
-  //set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-  // add note to json file and notes array in this function
-  const note = createNewNote(req.body, notes);
-  res.json(note);
-});
+// app.post("/api/db", (req, res) => {
+//   // req.body is where our incoming content will be
+//   console.log(req.body);
+//   //set id based on what the next index of the array will be
+//   req.body.id = notes.length.toString();
+//   // add note to json file and notes array in this function
+//   const note = createNewNote(req.body, notes);
+//   res.json(note);
+// });
