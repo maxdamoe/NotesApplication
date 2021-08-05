f3d60863f2e8b419846cb8ebea73e66b55001747
msimp <max.simpson@gmail.com>
2021-08-04 22:31:31 -0500

    please?

--- server.js
+++ server.js
@@ -1,47 +1,137 @@
+// SET UP DEPENDENCIES & BOILERPLATE //
+const fs = require("fs");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require("fs")
-const path = require("path")
 
-//parse incoming string or array data
-app.use(express.urlencoded({extended: true}));
-//parse incoming JSON data
+// IMPORT THE "PATH" module here //
+const path = require("path");
+//// SET VARIABLE THAT REQUIRES DB FILE TO ACCESS DATA //
+const userNotes = require("./db/db.json")
+
+//// set up Express app to handle data parsing ////
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// END BOILERPLATE & DEPENDENCIES //
 
-const notesArray = [];
-
-
-
-
-
-//HTML Routes
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+/// ROUTES START HERE ///////////////
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+/// GET /NOTES SHOULD RETURN THE NOTES.HTML FILE
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 
-// API Routes
- app.get('/api/notes', (req, res) => {
-   return res.json(notesArray)
- });
+//// GET /api/notes should read the db.json file and return all saved notes json
+app.get("/api/notes", (req, res) => {
+    console.log(userNotes);
+    return res.json(userNotes);   /// will return all userNotes in json format 
 
- app.post('/api/notes', (req, res) => {
- const note = req.body;
- note.id = notesArray.length.toString();
- notesArray.push(note);
 
- fs.writeFile('data/db.json', JSON.stringify(notesArray), (err) => {
-   if (err) console.log(err);
- })
-  res.json(notesArray);
+});
+
+
+// POST /API/NOTES SHOULD RECEIVE A NEW NOTE TO SAVE ON THE REQUEST BODY AND ADD IT TO DB.JSON FILE, & RETURN NEW NOTE TO CLIENT 
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = (userNotes.length).toString();
+
+    userNotes.push(newNote);
+
+    // WRITE FILE, STRINGIFY USERNOTES ARRAY, AND LOG ERROR IF THERE 
+    fs.writeFile(notes, JSON.stringify(userNotes), (err) => {
+        if (err)
+            console.log(err);
+    })
+    res.json(newNote);
 })
+
+// app.delete("api/notes/:id", (req, res) => {
+//     for (let i = 0; i < userNotes.length; i++) {
+//         let deleteNote = userNotes[i];
+
+
+// }
+
+
+// THE * ROUTE WILL RETURN USER TO HOME PAGE AND SHOULD ALWAYS COME AFTER OTHER ROUTES THAT 'GET' OR IT WILL TAKE PRECEDENCE OVER THE OTHERS. // 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+// APP IS LISTENING TO PORT //
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const fs = require("fs")
+// const path = require("path")
+
+// //parse incoming string or array data
+// app.use(express.urlencoded({extended: true}));
+// //parse incoming JSON data
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// const notesArray = [];
+
+
+
+
+
+// //HTML Routes
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"))
+// });
+
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/notes.html"))
+// });
+
+
+// // API Routes
+//  app.get('/api/notes', (req, res) => {
+//    return res.json(notesArray)
+//  });
+
+//  app.post('/api/notes', (req, res) => {
+//  const note = req.body;
+//  note.id = notesArray.length.toString();
+//  notesArray.push(note);
+
+//  fs.writeFile('data/db.json', JSON.stringify(notesArray), (err) => {
+//    if (err) console.log(err);
+//  })
+//   res.json(notesArray);
+// })
 
 
 
